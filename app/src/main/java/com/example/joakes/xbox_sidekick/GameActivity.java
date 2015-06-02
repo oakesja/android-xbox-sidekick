db99b8fda233912d48153f869b2f1651b196e48d
Jacob Oakes <jacoboakes92@gmail.com>
2015-06-02 18:51:25 -0400

    Made the profile view a header for the recyclerview

--- app/src/main/java/com/example/joakes/xbox_sidekick/GameActivity.java
+++ app/src/main/java/com/example/joakes/xbox_sidekick/GameActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bartoszlipinski.recyclerviewheader.RecyclerViewHeader;
 import com.example.joakes.xbox_sidekick.models.XboxGame;
 import com.example.joakes.xbox_sidekick.models.XboxProfile;
 import com.example.joakes.xbox_sidekick.views.ImageTextView;
@@ -24,33 +25,52 @@ public class GameActivity extends AppCompatActivity {
     EventBus eventBus;
     @Inject
     WebService webService;
-
-    @InjectView(R.id.profile_name_textview)
-    TextView profileNameTextView;
-    @InjectView(R.id.gamerscore_image_textview)
-    ImageTextView gamerscoreImageTextView;
-    @InjectView(R.id.gamer_picture)
-    ImageView gamerPicture;
     @InjectView(R.id.games_list)
     RecyclerView gamesList;
 
     private XboxGameAdapter mAdapter;
+    public ImageView gamerPicture;
+    public ImageTextView gamerscoreImageTextView;
+    public TextView profileNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupActivity();
+        makeRequests();
+        setupRecylerView();
+        setRecyclerViewHeader();
+    }
+
+    private void setupActivity() {
         setContentView(R.layout.activity_game);
         ButterKnife.inject(this);
-
         ((BaseApplication) getApplication()).component().inject(this);
-        webService.getProfile();
-        webService.getGameList();
+    }
 
+    private void setupRecylerView() {
         gamesList.setHasFixedSize(true);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         gamesList.setLayoutManager(layoutManager);
         mAdapter = new XboxGameAdapter(this);
         gamesList.setAdapter(mAdapter);
+    }
+
+    private void makeRequests() {
+        webService.getProfile();
+        webService.getGameList();
+    }
+
+    private void setRecyclerViewHeader() {
+        RecyclerViewHeader header = RecyclerViewHeader.fromXml(this, R.layout.profile_view);
+        header.attachTo(gamesList);
+        setProfileViews();
+    }
+
+    private void setProfileViews() {
+        gamerPicture = (ImageView) findViewById(R.id.gamer_picture);
+        gamerscoreImageTextView = (ImageTextView) findViewById(R.id.gamerscore_image_textview);
+        profileNameTextView = (TextView) findViewById(R.id.profile_name_textview);
     }
 
     @Override
@@ -71,6 +91,7 @@ public class GameActivity extends AppCompatActivity {
         profileNameTextView.setText(profile.getGamertag());
         gamerscoreImageTextView.setImageAndTextIfValid(profile.getGamerscore(), R.drawable.ic_gamerscore);
     }
+
 
     public void onEvent(ArrayList<XboxGame> games) {
         mAdapter.addGames(games);
